d6aa4a600eb014e47405e3a1aadc82b7fa357058
purch <burcinguzel@engineer.com>
2016-09-09 08:37:40 +0000

    Some corrections

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.get('/login/callback', function(req, res, next) {
                     req.session.oauth.access_token_secret = oauth_access_token_secret;
                     req.session.username = results.screen_name;
                     res.send("Authentication Successful");
-                    res.redirect('/');
+                    res.render("index",{username:req.session.username});    
                 }
             }
         );
